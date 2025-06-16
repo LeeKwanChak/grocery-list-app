a7543d51efc3380e0cb9ab7f5248e1b064ad91ac
LEE Kwan Chak <leechak330@gmail.com>
2025-06-17 03:05:43 +0800

    Initial commit

---
+++ grocerylist/src/test/java/com/marcus/grocerylist/GrocerylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcus.grocerylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
